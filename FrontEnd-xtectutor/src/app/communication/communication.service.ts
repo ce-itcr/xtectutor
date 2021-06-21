9f5dfd3e08969e5bb3dd5d156f2a42f1b9ca67ed
angelortizv <angelo2898@gmail.com>
2021-06-21 16:31:50 -0600

    [ADD] Create categories

--- FrontEnd-xtectutor/src/app/communication/communication.service.ts
+++ FrontEnd-xtectutor/src/app/communication/communication.service.ts
@@ -10,12 +10,12 @@ export class CommunicationService {
   constructor(private http: HttpClient, private router: Router) { }
 
   getStudentData(username){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/username/"+username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/username/"+username;
     return this.http.get(url);
   }
 
   getStudentEntries(username){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/entriesdb/username/"+username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/entriesdb/username/"+username;
     return this.http.get<any>(url).subscribe(res => {
       var data = [];
       var cont = 0;
@@ -30,24 +30,24 @@ export class CommunicationService {
   }
 
   updateStudentPassword(username, password){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/userdb/username/"+ username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/userdb/username/"+ username;
     let body = {"password":password};
     return this.http.patch(url, body);
   }
 
   getAdminData(username){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/admindb/username/"+username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/admindb/username/"+username;
     return this.http.get(url);
   }
 
   updateAdminPassword(username, password){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/admindb/username/"+ username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/admindb/username/"+ username;
     let body = {"password":password};
     return this.http.patch(url, body);
   }
 
   getAdminsList(){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/admindb";
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/admindb";
     return this.http.get<any>(url).subscribe(res => {
       var data = [];
       var cont = 0;
@@ -62,14 +62,65 @@ export class CommunicationService {
   }
 
   createAdmin(username,password,adminName,mail, campus){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/admindb";
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/admindb";
     let body = {"username":username,"password":password,"userType":"admin","adminName":adminName,"mail":mail,"campus":campus};
     return this.http.post(url, body);
   }
 
   removeAdmin(username){
-    let url = "https://sheet.best/api/sheets/d438c9c1-8af2-40a2-a4b3-c4cfa7e375e7/tabs/admindb/username/"+username;
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/admindb/username/"+username;
     return this.http.delete(url);
   }
+
+  getCareers(){
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/careersdb";
+    return this.http.get<any>(url).subscribe(res => {
+      var data = [];
+      var cont = 0;
+      while(cont < res.length){
+        data.push(res[cont]);
+        cont++;
+      }
+      localStorage.setItem("careersList", JSON.stringify(data));
+    }, error => {
+      alert("Error al obtener lista de administradores")
+    })
+  }
+
+  getCourses(){
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/coursesdb";
+    return this.http.get<any>(url).subscribe(res => {
+      var data = [];
+      var cont = 0;
+      while(cont < res.length){
+        data.push(res[cont]);
+        cont++;
+      }
+      localStorage.setItem("coursesList", JSON.stringify(data));
+    }, error => {
+      alert("Error al obtener lista de administradores")
+    })
+  }
+
+  createCareer(careerName){
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/careersdb";
+    let body = {"careerName":careerName};
+    return this.http.post(url, body);
+  }
+
+  createCourse(courseName,courseCode,associatedCareer){
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/coursesdb";
+    let body = {"category":"course","name":courseName,"code":courseCode,"associatedCareer":associatedCareer};
+    return this.http.post(url, body);
+  }
+
+  createSubject(subjectName,associatedCourse){
+    let url = "https://sheet.best/api/sheets/2bd3dc67-3b3e-4429-81db-5a9a70755379/tabs/subjectsdb";
+    let body = {"category":"subject","subjectName":subjectName,"associatedCourse":associatedCourse};
+    return this.http.post(url, body);
+  }
+
+
+
 
 }
--- FrontEnd-xtectutor/src/app/pages/admin-pages/categories/categories.component.ts
+++ FrontEnd-xtectutor/src/app/pages/admin-pages/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommunicationService } from 'app/communication/communication.service';
 
 
 @Component({
@@ -9,8 +11,45 @@ import { Component, OnInit } from '@angular/core';
 
 export class CategoriesComponent implements OnInit{
 
+    careersData;
+    coursesData;
+
+
+    constructor(private modal:NgbModal, private CS:CommunicationService){}
+
     ngOnInit(){
-      
+      this.CS.getCareers();     
+      this.CS.getCourses(); 
     }
 
+    openModal(content){ this.modal.open(content,{size:'ms', centered:true});}
+
+    createCareer(careerName){
+        this.CS.createCareer(careerName).subscribe(res => {
+            console.log(res);
+        })
+    }
+    
+    createCourse(courseName,courseCode,associatedCareer){
+        //alert(courseName + courseCode +associatedCareer)
+        this.CS.createCourse(courseName,courseCode,associatedCareer).subscribe(res => {
+            console.log(res);
+        })
+    }
+
+    createSubject(subjectName,associatedCourse){
+        this.CS.createSubject(subjectName,associatedCourse).subscribe(res => {
+            console.log(res);
+        })
+    }
+
+    setCareers(){
+        var localData = localStorage.getItem("careersList");
+        this.careersData = JSON.parse(localData);
+    }
+
+    setCourses(){
+        var localData = localStorage.getItem("coursesList");
+        this.coursesData = JSON.parse(localData);   
+    }
 }
