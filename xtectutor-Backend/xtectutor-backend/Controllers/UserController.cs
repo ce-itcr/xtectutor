899ed0a677c9da63e2365e8727074eddda290080
Jonathan Esquivel <jesquivelsa@gmail.com>
2021-06-26 22:06:29 -0600

    [ADD] Get user/admin info

---
+++ xtectutor-Backend/xtectutor-backend/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Web.Http.Cors;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace xtectutor_backend.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200/", headers: "*", methods: "*")]
+
+    public class UserController : ApiController
+    {
+        //static string stringconnection = @"Data Source=DESKTOP-RCFSH5R\MSSQLSERVER05;Initial Catalog=xtectutor;Integrated Security=True";
+        static string stringconnection = @"Data Source=MELI\SQLEXPRESS;Initial Catalog=xtectutor;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(stringconnection);
+
+        [HttpPost]
+        [Route("api/user/get/info")]
+        public JArray getUserInfo([FromBody] JObject UserInfo)
+        {
+
+            conn.Open();
+            SqlCommand selectRequest = conn.CreateCommand();
+            selectRequest.CommandText = "EXEC sp_GetUserInfo @Username";
+            selectRequest.Parameters.Add("@Username", SqlDbType.VarChar, 10).Value = UserInfo["username"];
+            selectRequest.ExecuteNonQuery();
+
+            SqlDataReader data = selectRequest.ExecuteReader();
+            JArray obj = new JArray();
+
+            while (data.Read())
+            {
+                JObject TYSUserInfo = new JObject(
+                new JProperty("username", data.GetValue(0).ToString()),
+                new JProperty("password", data.GetValue(1).ToString()),
+                new JProperty("userType", data.GetValue(2).ToString()),
+                new JProperty("fullName", data.GetValue(3).ToString()),
+                new JProperty("mail", data.GetValue(4).ToString()),
+                new JProperty("campus", data.GetValue(5).ToString())
+                );
+                obj.Add(TYSUserInfo);
+            }
+            data.Close();
+            conn.Close();
+            return obj;
+        }
+    }
+}
